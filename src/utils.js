40620c2ab22d4dfedc61bb63e613927d592af837
Trevor Slaton <trevor.slaton@gmail.com>
2017-12-30 14:54:56 -0800

    fix: Reduce applied with incorrect signature, reversing order of authors

--- src/utils.js
+++ src/utils.js
@@ -1,10 +1,10 @@
 import _ from 'lodash'
 
-export const formatBookResults = results => 
+export const formatBookResults = results =>
   _.sortBy(results.map(b => ({
     id: b.id,
     title: b.title || '',
-    authors: (b.authors || []).reduce((author, acc) => author ? `${acc}, ${author}` : acc, ''),
+    authors: (b.authors || []).reduce((acc, author) => acc ? `${acc}, ${author}` : author, ''),
     coverURL: (b.imageLinks || {}).thumbnail || '',
     shelf: b.shelf || 'none',
   })), 'title')
